c8416fe61c7cb6d598668b901cb742e14a7e594d
stan-is-laus <stanguldemond@gmail.com>
2017-10-20 17:24:43 +0200

    deleted unused js and xml

--- src/topics/network-layer.js
+++
@@ -1,8 +0,0 @@
-import NetworkLayerDiagram from '../diagrams/network-layer.xml';
-
-export default {
-  title: 'Network Layer',
-  description: 'Relay’s Network Layer does the work of exchanging data with the GraphQL server. It’s also configurable, so this would be a good place to do something like modify the headers of your requests.',
-  officialDocs: 'https://facebook.github.io/relay/docs/guides-network-layer.html#content',
-  diagram: NetworkLayerDiagram
-}
--- src/topics/object-type.js
+++
@@ -1,9 +0,0 @@
-import ObjectTypeDiagram from '../diagrams/object-type.xml';
-import ObjectTypeFields from './object-type-fields';
-
-export default {
-  title: 'Object Type',
-  description: 'This is your generic object type. It could be anything in your data model, such as a user, a blog post or product review.',
-  diagram: ObjectTypeDiagram,
-  children: ObjectTypeFields
-}
